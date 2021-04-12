b1f74a75ffa732fe5a1dd0f4ea5543ce82e7807c
navyCK <ckck81026@gmail.com>
2021-04-12 23:04:15 +0900

    210412

--- 9. 최단 경로/9-2.py
+++ 9. 최단 경로/9-2.py
@@ -6,3 +6,41 @@
 # 우선순위 큐 - 우선순위가 가장 높은 데이터를 가장 먼저 삭제
 # 현재 가장 가까운 노드를 저장하기 위한 목적으로만 우선순위 큐를 추가로 이용한다고 보자.
 
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)
+
+n, m = map(int, input().split())
+start = int(input())
+graph = [[] for i in range(n + 1)]
+distance = [INF] * (n + 1)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+for i in range(1, n + 1):
+    if distance[i] == INF:
+        print("INFINITY")
+    else:
+        print(distance[i])
+
+
+# 이 코드를 바로 이해하기엔 어렵지만, 다익스트라 최단 경로 알고리즘의 소스 코드만 잘 기억해보자.
